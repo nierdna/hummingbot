d927e4c7c25d0de162b6e3418a0974de74585191
nierdna <cassie382.618@gmail.com>
2025-05-13 21:58:05 +0700

    update

--- hummingbot/client/performance.py
+++ hummingbot/client/performance.py
@@ -256,11 +256,11 @@ class PerformanceMetrics:
                 last_price = await RateOracle.get_instance().stored_or_live_rate(rate_pair)
                 if last_price is not None:
                     self.fee_in_quote += fee_amount * last_price
-                else:
-                    self.logger().warning(
-                        f"Could not find exchange rate for {rate_pair} "
-                        f"using {RateOracle.get_instance()}. PNL value will be inconsistent."
-                    )
+                # else:
+                    # self.logger().warning(
+                    #     f"Could not find exchange rate for {rate_pair} "
+                    #     f"using {RateOracle.get_instance()}. PNL value will be inconsistent."
+                    # )
 
     def _calculate_trade_pnl(self, buys: list, sells: list):
         self.trade_pnl = self.cur_value - self.hold_value
